b40f7d08660abc46b64a170db0f914b5294bcb7f
lekiagospel@gmail.com <lekiagospel@gmail.com>
2019-12-17 15:45:14 +0100

    refactor and restructure

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -11,25 +11,38 @@ class BooksController extends Controller
    {
       //$this-> validateRequest();
 
-       Book::create($this-> validateRequest());
-       
+       $book = Book::create($this-> validateRequest());
+
     //Book::create(request(['title', 'author']));
+
+        return redirect($book->path());
+
    }
 
     public function update(Book $book)
     {
+        $book->update( $this->validateRequest());
 
-            $book->update( $this->validateRequest());
+        return redirect($book->path());
+    }
+
+    public function destroy(Book $book)
+    {
+            $book->delete();
+
+            return redirect('/books');
     }
 
     protected function validateRequest()
     {
 
-    return $this->validate(request(), [
+         return $this->validate(request(), [
 
             'title' => 'required',
             'author' => 'required',
         ]);
 
     }
+
+
 }
